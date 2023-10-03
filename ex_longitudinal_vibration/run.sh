93812116c76ca59c065e662eff34ac193cc10241
HuangYin <1071324110@qq.com>
2023-10-03 20:52:45 +0800

    fix

--- ex_longitudinal_vibration/run.sh
+++
@@ -1,7 +0,0 @@
-####################################################################################
-#
-# commands
-#
-####################################################################################
-
-python ex_single_pendulum1/train.py --config /home/lbu/project/pixels_hnn/ex_single_pendulum1/config.py
